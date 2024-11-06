9f675c1b2942f93e7d3df3b32ee3cec152be0277
Sai Chanadana <tsaichandana@sloperateinc.com>
2024-11-06 13:03:51 +0530

    Signup feature has been added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import './App.css';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home';
 import Header from './Components/Header';
-import SignIn from './Components/SignIn'; // Corrected the import for SignIn component
+import SignIn from './Components/SignIn';
+import SignupPage from './SignUp/SignupPage';
 
 function App() {
   return (
@@ -11,11 +14,12 @@ function App() {
         <Header />
         <Routes>
           <Route path="/home" element={<Home />} />
-          <Route path="/signin" element={<SignIn />} /> {/* Added route for SignIn */}
+          <Route path="/signin" element={<SignIn />} /> 
+          <Route path="/signup" element={<SignupPage/>} />
         </Routes>
       </div>
     </Router>
-  );
+  )
 }
 
 export default App;

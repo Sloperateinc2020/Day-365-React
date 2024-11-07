f0bfc90af243cb9bec9cca62e83e77d31f496fb4
Sai Chanadana <tsaichandana@sloperateinc.com>
2024-11-07 17:22:39 +0530

    enhancement of config js, home files,app js and added new file booking

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './Components/Home';
 import Header from './Components/Header';
 import SignIn from './Components/SignIn';
 import SignupPage from './SignUp/SignupPage';
+import Booking from './Components/Booking/Booking';
 
 function App() {
   return (
@@ -13,13 +14,16 @@ function App() {
       <div className="min-h-screen bg-gray-50">
         <Header />
         <Routes>
+          {/* Redirect the root path to /home */}
+          <Route path="/" element={<Navigate to="/home" />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/signin" element={<SignIn />} /> 
-          <Route path="/signup" element={<SignupPage/>} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/booking" element={<Booking />} />
         </Routes>
       </div>
     </Router>
-  )
+  );
 }
 
 export default App;
--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 const config = {
-    SERVICE_API_URL: 'https://run.mocky.io/v3/8970dcbe-05f2-4d63-8ec4-16f8cb2b5b59',
+    SERVICE_API_URL: 'https://run.mocky.io/v3/7642aac1-52ee-4b85-a046-5662934174f0',
     LATESTSERVICE_API_URL: 'https://run.mocky.io/v3/6e66ba44-034d-424c-86a0-fed2b8f00cbf',
-    TOPSERVICE_API_URL:'https://run.mocky.io/v3/d97271e9-bfff-462e-99c3-2ff08becbb94'
+    TOPSERVICE_API_URL:'https://run.mocky.io/v3/11b3935e-3cd3-4b46-bd5b-d8781e8d2b7e',
+    BOOKING_API_URL:'https://run.mocky.io/v3/cafac388-fe39-48de-964f-2871722efcb8'
 };
 
 export default config;

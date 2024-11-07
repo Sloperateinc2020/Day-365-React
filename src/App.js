0eef4dad1efbf7653320298bcec98e4243c5cbff
unknown <gangineedibhanuprakash@gmail.com>
2024-11-07 22:54:40 +0530

    devloped availabilitypage

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './Components/Home';
 import Header from './Components/Header';
 import SignIn from './Components/SignIn';
 import SignupPage from './SignUp/SignupPage';
+import Availability from './Availability/Availability'; // Import the Availability component
 
 function App() {
   return (
@@ -13,14 +14,27 @@ function App() {
       <div className="min-h-screen bg-gray-50">
         <Header />
         <Routes>
-          <Route path="/home" element={<Home />} />
+          {/* Home Route */}
+          <Route path="/" element={<Home />} />
+          
+          {/* SignIn Route */}
           <Route path="/signin" element={<SignIn />} /> 
-          <Route path="/signup" element={<SignupPage/>} />
-          <Route path='/about' element={<AboutPage />} />
+          
+          {/* SignUp Route */}
+          <Route path="/signup" element={<SignupPage />} />
+          
+          {/* About Page Route */}
+          <Route path="/about" element={<AboutPage />} />
+
+          {/* Availability Route */}
+          <Route path="/availability" element={<Availability />} />
+
+          {/* Default Route - to handle undefined paths */}
+          <Route path="*" element={<h1>404 - Page Not Found</h1>} />
         </Routes>
       </div>
     </Router>
-  )
-
+  );
 }
+
 export default App;
